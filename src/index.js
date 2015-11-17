1426bca06a81e311e92876bffddc28f63f238c8e
Javier Cejudo <javier@javiercejudo.com>
2015-11-18 00:30:08 +1100

    refactor: use purpose-made to-decimal pkg instead of composition

--- src/index.js
+++ src/index.js
@@ -2,26 +2,16 @@
 
 'use strict';
 
-var lcFactory = require('linear-converter');
 var unitScale = require('unit-scale');
-var unitPreset = require('unit-preset');
+var presetToDecimalFactory = require('linear-preset-to-decimal');
 
 module.exports = function factory(Decimal) {
-  var lc = lcFactory(Decimal);
+  var presetToDecimal = presetToDecimalFactory(Decimal);
 
   return function coefficients(a, b) {
     var decimalA = new Decimal(a.toString());
     var decimalB = new Decimal(b.toString());
 
-    return lc.composeConversions(
-      [
-        unitScale,
-        [
-          decimalB,
-          decimalA.plus(decimalB)
-        ]
-      ],
-      unitPreset
-    );
+    return presetToDecimal([unitScale, [decimalB, decimalB.plus(decimalA)]]);
   };
 };
