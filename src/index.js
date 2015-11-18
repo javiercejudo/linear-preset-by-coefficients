f4e47ac31222e17dc45e380ad948c239c614f728
Javier Cejudo <javier@javiercejudo.com>
2015-11-18 23:38:41 +1100

    refactor: use to decimal

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@
 
 var unitScale = require('unit-scale');
 var presetToDecimalFactory = require('linear-preset-to-decimal');
+var toDecimalFactory = require('to-decimal-arbitrary-precision');
 
 module.exports = function factory(Decimal) {
   var presetToDecimal = presetToDecimalFactory(Decimal);
+  var toDecimal = toDecimalFactory(Decimal);
 
   return function coefficients(a, b) {
-    var decimalA = new Decimal(a.toString());
-    var decimalB = new Decimal(b.toString());
+    var decimalA = toDecimal(a);
+    var decimalB = toDecimal(b);
 
     return presetToDecimal([unitScale, [decimalB, decimalB.plus(decimalA)]]);
   };
