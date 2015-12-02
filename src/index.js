aa1c277dfeb96bf2b37031c0a9d18dc6d7dcc6eb
Javier Cejudo <javier@javiercejudo.com>
2015-12-02 20:35:20 +1100

    refactor: use scale to decimal instead of preset to decimal

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@
 'use strict';
 
 var unitScale = require('unit-scale');
-var presetToDecimalFactory = require('linear-preset-to-decimal');
+var scaleToDecimalFactory = require('linear-scale-to-decimal');
 var toDecimalFactory = require('to-decimal-arbitrary-precision');
 
 module.exports = function factory(Decimal) {
-  var presetToDecimal = presetToDecimalFactory(Decimal);
+  var scaleToDecimal = scaleToDecimalFactory(Decimal);
   var toDecimal = toDecimalFactory(Decimal);
+  var unitScaleDecimal = scaleToDecimal(unitScale);
 
   return function coefficients(a, b) {
     var decimalA = toDecimal(a);
     var decimalB = toDecimal(b);
 
-    return presetToDecimal([unitScale, [decimalB, decimalB.plus(decimalA)]]);
+    return [unitScaleDecimal, [decimalB, decimalB.plus(decimalA)]];
   };
 };
